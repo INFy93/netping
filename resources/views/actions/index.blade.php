38624a4f79406e3d5eefa34b9c40de97d03aba15
INFy93 <infy93@gmail.com>
2022-02-19 12:00:13 +0300

    fix action pages views

--- resources/views/actions/index.blade.php
+++ resources/views/actions/index.blade.php
@@ -18,9 +18,6 @@
             <th
                 class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border dark:bg-gray-700 dark:text-gray-400 border-gray-300 hidden lg:table-cell">
                 Действие</th>
-                <th
-                class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 dark:bg-gray-700 dark:text-gray-400 hidden lg:table-cell">
-                </th>
         </tr>
     </thead>
     <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-500 dark:divide-gray-600">
@@ -34,13 +31,8 @@
             <td class="w-full lg:w-auto p-3 text-gray-800 dark:text-gray-100 text-center border border-b block lg:table-cell relative lg:static">
                 <span
                     class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Действие</span>
-                   {{ $action->name }}
-            </td>
-            <td class="w-full lg:w-auto p-3 text-gray-800 dark:text-gray-100 text-center border border-b block lg:table-cell relative lg:static">
-                <span
-                    class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"></span>
                     <a href="{{ route('edit_action', $action->id) }}"
-                        class="text-blue-600 dark:text-blue-200 hover:text-blue-900 dark:hover:text-blue-300 hover:text-blue-800 underline">Редактировать действие</a>
+                        class="text-blue-600 dark:text-blue-200 hover:text-blue-900 dark:hover:text-blue-300 hover:text-blue-800 underline">{{ $action->name }}</a>
             </td>
         </tr>
         @endforeach
